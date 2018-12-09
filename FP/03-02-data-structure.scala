a348736703eccde8d28b7d65c9814aa5a4be3d62
Hervito <herve.ahleung@gmail.com>
2018-12-09 15:39:22 +0000

    Drop and DropWhile functions

--- FP/03-02-data-structure.scala
+++ FP/03-02-data-structure.scala
@@ -40,6 +40,38 @@ object MyList { // `MyList` companion object. Contains functions for creating an
       case Cons(_head, tail) => Cons(newHead, tail)
       case _ => list
     }
+
+  def drop[A](list: MyList[A], n: Int): MyList[A] =
+    n match {
+      case 0 => list
+      case _ =>
+        val newList = tail(list)
+        drop(newList, n - 1)
+    }
+
+  def dropWhile[A](list: MyList[A], f: A => Boolean): MyList[A] =
+    list match {
+      case Cons(head, tail) =>
+        f(head) match {
+          case true => dropWhile(tail, f)
+          case false => Cons(head, dropWhile(tail, f))
+        }
+      case _ => Nil
+    }
+
+  def goodDropWhile[A](list: MyList[A])(f: A => Boolean): MyList[A] =
+    list match {
+      case Cons(head, tail) if f(head) => dropWhile(tail, f)
+      case _ => list
+    }
+
+  def init[A](list: MyList[A]): MyList[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(_, Nil) => Nil
+      case Cons(head, tail) => Cons(head, init(tail))
+    }
+  }
 }
 
 object Runner {
@@ -54,5 +86,18 @@ object Runner {
 
     val setHeadTesting = MyList.setHead(42, aList)
     println(setHeadTesting)
+
+    val dropTesting = MyList.drop(aList, 2)
+    println(dropTesting)
+
+    val otherList = MyList("wow", "no", "wow", "yes")
+    val wowFunction = (element: String) => element == "wow"
+    val dropWhileTesting = MyList.dropWhile(otherList, wowFunction)
+    println(dropWhileTesting)
+    val goodDropWhileTesting = MyList.goodDropWhile(otherList)(x => x == "wow")
+    println(goodDropWhileTesting)
+
+    val initTesting = MyList.init(otherList)
+    println(otherList)
   }
 }
