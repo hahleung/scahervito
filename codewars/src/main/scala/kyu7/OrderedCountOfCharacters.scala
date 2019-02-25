449a3acd4b9d359cc1f741803446c9d3505196c4
Hervé Ah-Leung <herve.ahleung@gmail.com>
2019-02-25 12:53:09 +0000

    Codewars: Ordered count of characters

--- codewars/src/main/scala/kyu7/OrderedCountOfCharacters.scala
+++ codewars/src/main/scala/kyu7/OrderedCountOfCharacters.scala
@@ -1,9 +1,14 @@
 package kyu7
 
+import scala.collection.mutable
+
 object OrderedCountOfCharacters {
   def orderedCount(chars: String): List[(Char, Int)] =
-    chars.foldLeft(Map.empty[Char, Int]) { (hashMap, char) =>
-      val newCounter = hashMap.getOrElse(char, 0) + 1
-      hashMap + (char -> newCounter)
+    chars.foldLeft(mutable.LinkedHashMap.empty[Char, Int]) { (listMap, char) =>
+      val newCounter = listMap.getOrElse(char, 0) + 1
+      listMap += (char -> newCounter)
     }.toList
 }
+
+// Concise, but multiple passes to the input
+//chars.distinct.map(c => (c, chars.count(_ == c))).toList
